5e6d3d4d9ae22cfdd25ce0f1ce77860af1e48971
Fabricio Ponce <fabricioponce2012@gmail.com>
2023-01-23 17:40:11 -0300

    Practica-HTML/CSS/JS

---
+++ JS/function.js
@@ -0,0 +1,6 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("Capturamos el evento clic")
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento"
+}
+
